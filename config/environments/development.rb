a2b00680930f097a3dd813f32151b37b8274b744
siti nuradila <startbeautiful@gmail.com>
2017-06-14 19:26:16 +0700

    set up ActionCable

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -48,6 +48,9 @@ config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  
+  # create javascript files instead of CoffeeScript files
+   config.app_generators.javascript_engine = :javascript
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
 
+ mount ActionCable.server => '/cable'
+ 
   resources :rooms do
    resources :messages
   end
