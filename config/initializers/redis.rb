eca17ae58bcf9e3d4336129ef17e2bd8c14a84f6
siti nuradila <startbeautiful@gmail.com>
2017-06-14 19:43:35 +0700

    fix redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,5 @@
-if Rails.env.Development?
-  redis = Redis.new(:host => 'localhost', :port => 6379)  
+if Rails.env.Production?
+  redis = Redis.new(url: ENV["REDIS_URL"])
+else
+  redis = Redis.new(:host => 'localhost', :port => 6379)
 end
